be79f5e93116dd999d6ae1d76ccdad74f4de9817
Sahil Shekh <95908470+sahilshekh@users.noreply.github.com>
2022-08-26 11:37:12 +0530

    successfully completed assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./components/Home/Home.jsx"
+import ContactsPage from './components/Contacts Page/ContactsPage';
+import Navbar from './components/navbar/Navbar';
+import { Routes ,Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar/>
+    
+    <Routes>
+    
+      <Route path='/' element={<Home/>}/>
+      <Route path='/contactpage' element={<ContactsPage/>}/>
+     </Routes>
+    
     </div>
   );
 }
---
+++ src/components/Contacts Page/ContactsPage.jsx
@@ -0,0 +1,68 @@
+  import React from 'react'
+  import { useState } from "react"
+  import "./Contact.css"
+  function ContactsPage() {
+    const [data,setData] =useState({
+        firstname:"",
+        lastname:"",
+         mobile:"",
+         email:""
+     
+     
+    })
+    const [result ,setResult] =useState([])
+      const HandleChange =(e)=>{
+        const name =e.target.name;
+        const value =e.target.value;
+        console.log(name,value)
+        setData({...data,[name]:value})
+
+           
+      }
+      const handle =(e)=>{
+    e.preventDefault()
+       const newUser = {...data, id:new Date().getDate().toString()}
+      setResult([...result,newUser])
+      setData({firstname:"",lastname:"",mobile:"",email:""})
+      }
+    return (
+        <div className='main'>
+    <form action="" onSubmit={handle}>
+    <div className="username">
+          <label className="name">FirstName</label><br></br>
+        <input type="text" name="firstname" maxlength="50" value={data.firstname} autoComplete="off" required className="input" onChange={HandleChange}/>
+</div>
+       
+       <div className="username">
+       <label className="name">LastName</label><br></br>
+        <input type="text" name="lastname" maxlength="50" value={data.lastname} autoComplete="off" required className="input" onChange={HandleChange}/>
+     
+</div>
+
+<div className="username">
+          <label className="name">Mobile No</label><br></br>
+        <input type="text" name="mobile" maxlength="10" value={data.mobile} autoComplete="off" required className="input" onChange={HandleChange}/>
+</div>
+<div className="username">
+<label className="name">Email ID</label><br></br>
+        <input type="text" name="email" value={data.email} autoComplete="off" required className="input" onChange={HandleChange}/>
+</div>
+
+   <button className="button" type="submit">Save</button>
+    </form>
+   <div className="div">
+  {result.map((e)=>(
+    <div key={e.id}>
+    <h3>FirstName: {e.firstname}</h3>
+    <h3>LastName: {e.lastname}</h3>
+    <h3>Mobile No: {e.mobile}</h3>
+    <h3>Email: {e.email}</h3>
+    </div>
+  ))}
+   </div>
+
+        </div>
+    )
+  }
+  
+  export default ContactsPage
